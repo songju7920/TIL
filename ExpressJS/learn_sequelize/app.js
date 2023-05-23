3ac3642b2b300e2c9e89bdfa25477c4863f47ef6
오송주 <dhthdwn7920@gmail.com>
2023-05-23 12:47:40 +0900

    Update

--- ExpressJS/learn_sequelize/app.js
+++ ExpressJS/learn_sequelize/app.js
@@ -1,30 +1,38 @@
+//필요한 파일들을 임포트 또는 디스턱처링(추출)하기
 const express = require("express");
 const { sequelize } = require("./models");
 const dotenv = require("dotenv");
 
+//config 파일 불러오기
 dotenv.config();
 
+//라우터(사용자 요청 처리 모듈) 불러오기
 const router = require("./router/index");
-
+//express 사용 가능한 상태로 설정
 const app = express();
-
+//포트 번호 설정
 const port = 8080;
 
+//미들웨어 설정하기
+//서버 요청이 Json파일일때 JavaScript 객체로 변환.
 app.use(express.json());
-//Querystring
+//서버 요청이 URL파일일때 JavaScript 객체로 변환하고, querystring해독을 내장 함수로 처리한다.
 app.use(express.urlencoded({ extended: false }));
-
+//엔드포인트 '/'가 들어오면 router실행하기
 app.use("/", router);
 
-app.listen("port", () => {
+//
+app.listen(port, () => {
   console.log(`Sever is litening on port ${port}!`);
 
-  //프로미스 알아봐
+  //기존 테이블을 삭제하지 않으면서 동기화
   sequelize
     .sync({ force: false })
+    //프로미스 성공시 -> 성공 메세지 터미널에 띄우기
     .then(() => {
       console.log(`Success to link DataBase!`);
     })
+    //프로미스 실패시 -> 오류 터미널에 띄우기
     .catch((err) => {
       console.error(err);
     });
--- ExpressJS/learn_sequelize/controller/write.js
+++ ExpressJS/learn_sequelize/controller/write.js
@@ -1,16 +1,23 @@
+//models에서 write만 디스턱처링(추출)해서 가져오기
 const { write } = require("../models");
 
-const createWriting = async (req, res) => {
+//비동기화 모듈 생성
+module.exports = createWriting = async (req, res) => {
+  //서버의 create와 update요청에서 writeID, writeHead, writeBody, writeUser를 추출해 가져오기
   const { writeID, writeHead, writeBody, writeUser } = req.body;
 
+  //아래 코드를 실행해보기
   try {
-    const haveID = await write.FindOne({
+    //writeID가 중복되는지 확인하기
+    const haveID = await write.findOne({
       where: { writeID },
     });
 
+    //중복된 경우 409 상태와 실패 메시지를 담은 JSON 응답 전송
     if (haveID)
       return res.status(409).json({ message: "중복된 아이디입니다." });
 
+    //DB의 write테이블에 추출한 데이터들을 담은 행이 생성되끼까지 대기한다
     await write.create({
       writeID,
       writeHead,
@@ -18,11 +25,15 @@ const createWriting = async (req, res) => {
       writeUser,
     });
 
+    //201 상태와 성공 메시지를 담은 JSON 응답 전송
     return res.status(201).json({
       message: "요청에 성공하였습니다.",
     });
   } catch (err) {
+    //위 코드에서 오류가 발생시
+    //터미널에 오류 메세지 출력
     console.error(err);
+    //400 상태와 실패 메시지를 담은 JSON 응답 전송.
     return res.status(400).json({
       message: "요청에 실패하였습니다.",
     });
